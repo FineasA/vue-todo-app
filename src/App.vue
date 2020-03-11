83843f5c11388bc53ab4e1e170159918da300c21
Fineas Anton <fineas.anton@gmail.com>
2020-03-11 13:08:27 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <div class="row justify-content-center">
+      <h2 class="title">Task Manager</h2>
+    </div>
+    <hr />
+    <to-do></to-do>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ToDo from "./components/ToDo.vue";
 
 export default {
-  name: "App",
   components: {
-    HelloWorld
+    ToDo
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.title {
+  font-family: "Roboto Condensed", sans-serif;
 }
 </style>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div class="row justify-content-center">
+      <input
+        type="text"
+        placeholder="Enter task..."
+        v-model="query"
+        @keyup.enter="addTask"
+      />
+    </div>
+    <br />
+    <div class="row justify-content-center">
+      <ul class="list-group">
+        <transition-group
+          enter-active-class="animated rollIn"
+          leave-active-class="animated bounceOut"
+        >
+          <li
+            class="list-group-item"
+            v-for="(task, index) in todos"
+            :key="index"
+            @click="deleteTask(index)"
+          >
+            {{ task.todo }}
+          </li>
+        </transition-group>
+      </ul>
+    </div>
+    <br />
+    <div class="row justify-content-center">
+      <div class="btn btn-primary" @click="addTask">Add Task</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      query: "",
+      task: {},
+      todos: []
+    };
+  },
+  methods: {
+    deleteTask(index) {
+      this.todos.splice(index, 1);
+    },
+    addTask() {
+      this.task = {
+        isCompleted: false,
+        todo: this.query
+      };
+      this.todos.push(this.task);
+      this.query = "";
+    }
+  }
+};
+</script>
+
+<style>
+.list-group-item {
+  cursor: pointer;
+}
+</style>
